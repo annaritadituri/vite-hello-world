7e273863507c7035681b83195d2753fdb23622a0
annaritadituri <annaritadituri@icloud.com>
2024-05-30 19:03:31 +0200

    Added jumbo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue'
 
 export default {
   components: {
     AppTitle,
+    AppHero,
   }
 }
 
@@ -12,7 +14,12 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero />
+  <p>Sono il resto della pagina, ma sono di colore rosso</p>
 </template>
 
 <style scoped>
+  p {
+    color: red;
+  }
 </style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  name: "AppHero",
+}
+
+</script>
+
+<template>
+  <h2>App Jumbo</h2>
+  <p>Paragrafo Jumbo</p>
+</template>
+
+<style scoped>
+  p {
+    color: green;
+  }
+</style>
